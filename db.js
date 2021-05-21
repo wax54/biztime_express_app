f030ad64f816f68f1c46479d28a08de0f358bee8
sam <wax654@gmail.com>
2021-05-21 14:05:55 -0700

    fixed DB bug

--- db.js
+++ db.js
@@ -1,2 +1,17 @@
 /** Database setup for BizTime. */
+const { Client } = require('pg');
+const { user, password, host, port} = require('./dbSecrets.json');
 
+let DB_URI = `postgresql://${user}:${password}@${host}:${port}/`;
+
+if (process.env.NODE_ENV === 'test'){
+    DB_URI += 'biztime_test';
+}else{
+    DB_URI += 'biztime';
+}
+
+let db = new Client({ connectionString: DB_URI });
+
+db.connect();
+
+module.exports = db;
