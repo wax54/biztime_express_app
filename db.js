8e81db0b4bba5163ec47055333e5c37627ff35f3
sam <wax654@gmail.com>
2021-05-30 14:36:05 -0700

    updated db.js to use default creds if no dbSecrets found

--- db.js
+++ db.js
@@ -2,7 +2,12 @@
 const { Client } = require('pg');
 const { user, password, host, port} = require('./dbSecrets.json');
 
-let DB_URI = `postgresql://${user}:${password}@${host}:${port}/`;
+let DB_URI = `postgresql://`;
+if (user){
+    DB_URI += `${user}:${password}@${host}:${port}/`;
+} else {
+    DB_URI += `/`;
+}
 
 if (process.env.NODE_ENV === 'test'){
     DB_URI += 'biztime_test';
